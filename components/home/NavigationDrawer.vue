9ab79c830e1c4dec7239146187448ade7969f0cf
GigaVardia <gigavardia@gmail.com>
2022-04-14 11:17:49 +0400

    added new items

--- components/home/NavigationDrawer.vue
+++ components/home/NavigationDrawer.vue
@@ -28,7 +28,14 @@
           exact
           sub-group
         >
-          {{ nestedItem.title }}
+          <v-list-item-icon class="mr-2">
+            <v-icon>mdi-circle-small </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ nestedItem.title }}
+            </v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list-group>
     </v-list>
@@ -39,7 +46,7 @@
 export default {
   data() {
     return {
-      listModel: [true, true],
+      listModel: [true, true, true, true],
       items: [
         {
           title: 'Front-End Development',
@@ -47,19 +54,23 @@ export default {
           items: [
             {
               title: 'All',
+              jobs: 63,
               routeParam: 'all-frontend',
             },
             {
               title: 'Vue.js',
               routeParam: 'vuejs',
+              jobs: 22,
             },
             {
               title: 'React/Next.js',
               routeParam: 'react-nuxtjs',
+              jobs: '28',
             },
             {
               title: 'Angular',
               routeParam: 'angular',
+              jobs: 13,
             },
           ],
         },
@@ -86,6 +97,53 @@ export default {
             {
               title: 'Ruby',
               routeParam: 'ruby',
+            },
+          ],
+        },
+        {
+          title: 'QA',
+          id: 'qa',
+          items: [
+            {
+              title: 'All',
+              params: 'qa-all',
+              jobs: 8,
+            },
+            {
+              title: 'Manual testing',
+              param: 'manual-testing',
+              jobs: 3,
+            },
+            {
+              title: 'Test automation',
+              param: 'test-automation',
+              jobs: 5,
+            },
+          ],
+        },
+        {
+          title: 'Operations',
+          id: 'operations',
+          items: [
+            {
+              title: 'All',
+              params: 'operations-all',
+              jobs: 15,
+            },
+            {
+              title: 'DevOps',
+              param: 'devops',
+              jobs: 7,
+            },
+            {
+              title: 'System administrator',
+              param: 'system-administrator',
+              jobs: 5,
+            },
+            {
+              title: 'Database administrator',
+              param: 'database-administrator',
+              jobs: 3,
             },
           ],
         },

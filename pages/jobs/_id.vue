5764f9f013532062d07f42651e0866f6c14f4d0b
GigaVardia <gigavardia@gmail.com>
2022-04-16 00:04:33 +0400

    job detail page test

--- pages/jobs/_id.vue
+++ pages/jobs/_id.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>job detail</div>
+  <v-container>
+    <v-row>
+      {{ job.title }}
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -9,8 +13,27 @@ export default {
     if (to.params.id) next()
     else next('/')
   },
-  mounted() {
-    console.log(this.$route)
+  async asyncData() {
+    const job = await new Promise((resolve) => {
+      // eslint-disable-next-line nuxt/no-timing-in-fetch-data
+      setTimeout(
+        () =>
+          resolve({
+            title: 'Full Stack Developer',
+            group: 'Web Development',
+            subGroup: 'Full Stack',
+            id: '112233',
+            date: Date.now(),
+            description: '',
+            salary: '3000-4000',
+            companyLogo:
+              'https://s3.eu-central-1.amazonaws.com/public.temicloud.io/images/uploads/d9f9c089-d258-4ae2-9ea7-0a5708404198.png',
+            tags: ['React', 'Nodejs', 'Git'],
+          }),
+        500
+      )
+    })
+    return { job }
   },
 }
 </script>

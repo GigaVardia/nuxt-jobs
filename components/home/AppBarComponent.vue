4cb44fa9848bec4510bc96382dee3c939208cc99
GigaVardia <gigavardia@gmail.com>
2022-04-20 22:10:31 +0400

    upload job icon in app bar

--- components/home/AppBarComponent.vue
+++ components/home/AppBarComponent.vue
@@ -8,6 +8,16 @@
       <div class="mr-4 d-flex align-center">
         <v-text-field hide-details placeholder="Search job..." />
       </div>
+      <div>
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <v-btn icon nuxt to="new-job" v-bind="attrs" v-on="on">
+              <v-icon> mdi-briefcase-upload </v-icon>
+            </v-btn>
+          </template>
+          <span>Upload job</span>
+        </v-tooltip>
+      </div>
       <div v-if="!isLoggedIn" class="d-flex align-center justify-center">
         <div>
           <LoginComponent v-if="true" class="mr-4" />
